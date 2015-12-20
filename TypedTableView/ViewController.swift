4407ee2ab27235d8723440c37a74e40c816f0cb0
AdamSliwakowski <adam.sliwakowski@wimdu.com>
2015-12-20 22:19:03 +0100

    Initial Commit

---
+++ TypedTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TypedTableView
+//
+//  Created by Adam Śliwakowski on 20.12.2015.
+//  Copyright © 2015 AdamSliwakowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
