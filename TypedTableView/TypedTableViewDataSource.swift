71d627d331360420382d62b0fdffbaaa95858354
AdamSliwakowski <adam.sliwakowski@wimdu.com>
2016-01-01 13:57:25 +0100

    Added documentation for TypedTableViewDataSource insert, remove etc. methods

--- TypedTableView/TypedTableViewDataSource.swift
+++ TypedTableView/TypedTableViewDataSource.swift
@@ -60,11 +60,24 @@ class TypedTableViewDataSource<T, K where K: UITableViewCell, K: TypedTableViewC
 }
 
 extension TypedTableViewDataSource {
+    /**
+     Sets tableView's data to array received in param and realods tableView without animation.
+     
+     - parameter objects: Array of all objects of specified type that will be populated to dataSource array.
+     */
     func reload(objects: [T]) {
         self.objects = objects
         tableView?.reloadData()
     }
     
+    /**
+     Sets tableView's data to array received in param and realods tableView with animation.
+     
+     - Removes all objects from dataSource array and all rows from tableView with animation.
+     - Inserts new objects to dataSource array with insertion of rows to tableView with animation.
+     
+     - parameter objects: Array of all objects of specified type that will be populated to tableView.
+     */
     func reloadWithAnimation(objects: [T]) {
         removeAll()
         insertContentsOf(contentsOf: objects, atIndexPath: NSIndexPath(forRow: 0, inSection: 0))
@@ -72,37 +85,68 @@ extension TypedTableViewDataSource {
 }
 
 extension TypedTableViewDataSource {
+    /**
+     Appends received object to dataSource array and inserts row into tableView.
+     
+     - parameter newElement: Object of specified type that will be appended to dataSource array.
+     */
     func append(newElement: T) {
         objects.append(newElement)
         performChange(.Insert, index: objects.endIndex)
     }
     
+    /**
+     Removes object at indexPath from dataSource array and deletes row from tableView.
+     
+     - parameter indexPath: TableView's indexPath that should be deleted from tableView and removed from dataSource.
+     */
     func removeAtIndexPath(indexPath: NSIndexPath) {
         objects.removeAtIndex(indexPath.row)
         performChange(.Delete, index: indexPath.row)
     }
     
+    /**
+     Removes first object from dataSource array and deletes row from tableView.
+     */
     func removeFirst() {
         objects.removeFirst()
         performChange(.Delete, index: objects.startIndex)
     }
     
+    /**
+     Removes last object from dataSource array and deletes row from tableView.
+     */
     func removeLast() {
         objects.removeLast()
         performChange(.Delete, index: objects.endIndex)
     }
     
+    /**
+     Removes all objects from dataSource array and deletes rows from tableView.
+     */
     func removeAll() {
         let indexPaths = [Int](0..<objects.count).map { NSIndexPath(forRow: $0, inSection: 0) }
         objects.removeAll()
         performChange(.Delete, indexPaths: indexPaths)
     }
     
+    /**
+     Inserts received object at specified indexPath to dataSource array and inserts row into tableView.
+     
+     - parameter newElement: Object of specified type that will be inserted into dataSource array.
+     - parameter indexPath: IndexPath that would be used to insert row into tableView and object into dataSource.
+     */
     func insert(newElement: T, atIndexPath indexPath: NSIndexPath) {
         objects.insert(newElement, atIndex: indexPath.row)
         performChange(.Insert, index: indexPath.row)
     }
     
+    /**
+     Inserts received objects at specified indexPath to dataSource array and inserts rows into tableView.
+     
+     - parameter newElement: Array of objects of specified type that will be inserted into dataSource array.
+     - parameter indexPath: IndexPath that would be used to insert rows into tableView and object into dataSource.
+     */
     func insertContentsOf(contentsOf newElements: [T], atIndexPath indexPath: NSIndexPath) {
         objects.insertContentsOf(newElements, at: indexPath.row)
         var indexPaths = [NSIndexPath]()
